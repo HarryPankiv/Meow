c40f87a90d9434c500b96db0308c51ef77460274
bohdankaminskyi <b.kaminskyi99@gmail.com>
2017-10-02 18:57:14 +0300

    added db model

--- news_explorer/models.py
+++ news_explorer/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    author = models.CharField(max_length=50)
+    title = models.CharField(max_length=100, unique=True)
+    description = models.CharField(max_length=200)
+    url = models.URLField(max_length=200)
+    url_to_image = models.URLField(max_length=200)
+    publish_date = models.DateTimeField()
